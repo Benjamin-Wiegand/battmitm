01948b8a7ad4d96984de5645e19deb7d239a76ea
Benjamin Wiegand <ben@noreply.git.wiegand.pw>
2025-08-29 18:59:01 -0700

    properly handle start/stop/abort

--- mitm.c
+++ mitm.c
@@ -26,21 +26,30 @@
 #include "status.h"
 #include "config.h"
 #include "static_queue.h"
-#include "pico/i2c_slave.h"
 #include "pico/stdlib.h"
+#include "hardware/irq.h"
 #include <stdio.h>
 
+enum i2c_transfer_event {
+    I2C_READ,
+    I2C_WRITE,
+    I2C_START,
+    I2C_STOP,
+    I2C_ABORT
+};
 
-struct smbus_transfer {
-    i2c_slave_event_t event;
+typedef enum i2c_transfer_event i2c_transfer_event_t;
+
+struct i2c_transfer {
+    i2c_transfer_event_t event;
     uint8_t data;
 };
 
-typedef struct smbus_transfer smbus_transfer_t;
+typedef struct i2c_transfer i2c_transfer_t;
 
 
-#define MITM_QUEUE_ELEMENT_SIZE sizeof(smbus_transfer_t)
-#define MITM_QUEUE_MAX_ELEMENTS 10
+#define MITM_QUEUE_ELEMENT_SIZE sizeof(i2c_transfer_t)
+#define MITM_QUEUE_MAX_ELEMENTS 100
 
 #define MITM_CMD_BUFFER_SIZE 64
 #define MITM_REPLY_BUFFER_SIZE 64
@@ -49,7 +58,7 @@ typedef struct smbus_transfer smbus_transfer_t;
 static_queue_t* mitm_transfer_queue;
 bool mitm_transfer_queue_overflow = false;
 
-i2c_slave_event_t previous_event = I2C_SLAVE_FINISH;
+i2c_transfer_event_t previous_event = I2C_ABORT;
 
 uint8_t mitm_cmd_buffer[MITM_CMD_BUFFER_SIZE];
 size_t mitm_cmd_buffer_index = 0;
@@ -58,17 +67,47 @@ uint8_t mitm_reply_buffer[MITM_REPLY_BUFFER_SIZE];
 size_t mitm_reply_buffer_index = 0;
 
 
-void mitm_laptop_irq_handler(i2c_inst_t* i2c, i2c_slave_event_t event) {
-    if (mitm_transfer_queue_overflow) return;
-
-    smbus_transfer_t* transfer = static_queue_add(mitm_transfer_queue);
+void mitm_laptop_on_i2c_event(i2c_transfer_event_t event) {
+    i2c_transfer_t* transfer = static_queue_add(mitm_transfer_queue);
     if (transfer == NULL) {
         mitm_transfer_queue_overflow = true;
         return;
     }
 
     transfer->event = event;
-    if (event == I2C_SLAVE_RECEIVE) i2c_read_raw_blocking(i2c, &transfer->data, 1);
+    if (event == I2C_WRITE) i2c_read_raw_blocking(LAPTOP_I2C, &transfer->data, 1);
+}
+
+void mitm_laptop_irq_handler() {
+    i2c_hw_t* hw = i2c_get_hw(LAPTOP_I2C);
+    uint32_t stat = hw->intr_stat;
+    if (stat == 0) return;
+    if (mitm_transfer_queue_overflow) return;
+
+    if (stat & I2C_IC_INTR_STAT_R_TX_ABRT_BITS) {
+        hw->clr_tx_abrt;
+        mitm_laptop_on_i2c_event(I2C_ABORT);
+    }
+
+    if (stat & I2C_IC_INTR_STAT_R_START_DET_BITS) {
+        hw->clr_start_det;
+        mitm_laptop_on_i2c_event(I2C_START);
+    }
+
+    if (stat & I2C_IC_INTR_STAT_R_STOP_DET_BITS) {
+        hw->clr_stop_det;
+        mitm_laptop_on_i2c_event(I2C_STOP);
+    }
+
+
+    if (stat & I2C_IC_INTR_STAT_R_RX_FULL_BITS) {
+        mitm_laptop_on_i2c_event(I2C_WRITE);
+    }
+
+    if (stat & I2C_IC_INTR_STAT_R_RD_REQ_BITS) {
+        hw->clr_rd_req;
+        mitm_laptop_on_i2c_event(I2C_READ);
+    }
 }
 
 
@@ -92,11 +131,21 @@ void mitm_init_i2c() {
     }
 
     i2c_set_slave_mode(LAPTOP_I2C, true, LAPTOP_I2C_ADDR);
-    i2c_slave_init(LAPTOP_I2C, LAPTOP_I2C_ADDR, &mitm_laptop_irq_handler);
+
+    // manually set up irq
+    uint i2c_irq = I2C0_IRQ + i2c_hw_index(LAPTOP_I2C);
+    i2c_get_hw(LAPTOP_I2C)->intr_mask = I2C_IC_INTR_MASK_M_RX_FULL_BITS | I2C_IC_INTR_MASK_M_RD_REQ_BITS | I2C_IC_INTR_MASK_M_TX_ABRT_BITS | I2C_IC_INTR_MASK_M_STOP_DET_BITS | I2C_IC_INTR_MASK_M_START_DET_BITS;
+    irq_set_exclusive_handler(i2c_irq, &mitm_laptop_irq_handler);
+    irq_set_enabled(i2c_irq, true);
 }
 
 void mitm_reinit_i2c() {
-    i2c_slave_deinit(LAPTOP_I2C);
+    uint i2c_irq = I2C0_IRQ + i2c_hw_index(LAPTOP_I2C);
+    irq_set_enabled(i2c_irq, false);
+    irq_remove_handler(i2c_irq, &mitm_laptop_irq_handler);
+    i2c_get_hw(LAPTOP_I2C)->intr_mask = I2C_IC_INTR_MASK_RESET;
+
+    i2c_set_slave_mode(LAPTOP_I2C, false, LAPTOP_I2C_ADDR);
     i2c_deinit(BATT_I2C);
     i2c_deinit(LAPTOP_I2C);
 
@@ -116,7 +165,7 @@ void mitm_loop() {
     i2c_dev_t* laptop = get_laptop_dev();
     i2c_dev_t* bms = get_bms_dev();
 
-    smbus_transfer_t* transfer = static_queue_peek(mitm_transfer_queue);
+    i2c_transfer_t* transfer = static_queue_peek(mitm_transfer_queue);
 
     if (mitm_transfer_queue_overflow) {
         printf("ERROR: mitm transfer queue overflow!!! please increase MITM_QUEUE_MAX_ELEMENTS\n");
@@ -134,10 +183,10 @@ void mitm_loop() {
         status_mitm(true);
         
         switch (transfer->event) {
-            case I2C_SLAVE_RECEIVE:
+            case I2C_WRITE:
 
                 // this should never happen
-                if (previous_event == I2C_SLAVE_REQUEST) {
+                if (previous_event == I2C_READ) {
                     printf("ERROR: no stop after read before write!!!\n");
                     mitm_reply_buffer[0] = 0;
                     i2c_write_raw_blocking(laptop->i2c, mitm_reply_buffer, 1);
@@ -159,10 +208,10 @@ void mitm_loop() {
                 printf("TX 0x%02x\n", mitm_cmd_buffer[mitm_cmd_buffer_index-1]);
 
                 break;
-            case I2C_SLAVE_REQUEST:
+            case I2C_READ:
 
                 // this should never happen
-                if (previous_event == I2C_SLAVE_RECEIVE) {
+                if (previous_event == I2C_WRITE) {
                     printf("ERROR: no stop after write before read!!!\n");
                     mitm_reply_buffer[0] = 0;
                     i2c_write_raw_blocking(laptop->i2c, mitm_reply_buffer, 1);
@@ -184,30 +233,43 @@ void mitm_loop() {
 
                 mitm_reply_buffer_index++;
                 break;
-            case I2C_SLAVE_FINISH:
-                //todo: need to replace the actual interrupt if I want this to be accurate, this impl is just guessing
-                if (previous_event == I2C_SLAVE_RECEIVE) {
+            case I2C_ABORT:
+            case I2C_STOP:
+                bool aborted = transfer->event == I2C_ABORT;
 
-                    bool is_cmd = mitm_cmd_buffer_index == 1;
-                    i2c_write_blocking(bms->i2c, bms->address, mitm_cmd_buffer + mitm_cmd_buffer_index - 1, 1, is_cmd); // cmd expects rx
-                    if (is_cmd) {
-                        printf("end of cmd\n");
-                    } else {
-                        printf("end of write (%d bytes)\n", mitm_cmd_buffer_index);
-                    }
-
-                } else if (previous_event == I2C_SLAVE_REQUEST) {
-                    i2c_stop_blocking(bms);
-                    printf("end of read (%d bytes)\n", mitm_reply_buffer_index);
+                if (previous_event == I2C_WRITE) {
+                    i2c_write_blocking(bms->i2c, bms->address, mitm_cmd_buffer + mitm_cmd_buffer_index - 1, 1, false);
+                    if (aborted) printf("ABORT - ");
+                    printf("end of TX (%d bytes)\n", mitm_cmd_buffer_index);
+                } else if (previous_event == I2C_READ) {
+                    i2c_stop_blocking(bms);     // pay the court a fine or serve your sentence
+                    if (aborted) printf("ABORT - ");
+                    printf("end of RX (%d bytes)\n", mitm_reply_buffer_index);
                 } else {
-                    printf("RESET\n");
+                    if (aborted) printf("ABORT\n");
+                    else printf("STOP\n");
                 }
-                
+
                 mitm_cmd_buffer_index = 0;
                 mitm_reply_buffer_index = 0;
                 break;
+            case I2C_START:
+
+                if (previous_event == I2C_WRITE) {
+                    i2c_write_blocking(bms->i2c, bms->address, mitm_cmd_buffer + mitm_cmd_buffer_index - 1, 1, true);
+                    printf("switching TX -> RX after sending (%d bytes)\n", mitm_cmd_buffer_index);
+                    mitm_cmd_buffer_index = 0;
+                } else if (previous_event == I2C_READ) {
+                    printf("START - end of RX (%d bytes)\n", mitm_reply_buffer_index);
+                    mitm_reply_buffer_index = 0;
+                } else {
+                    printf("START\n");
+                    mitm_cmd_buffer_index = 0;
+                    mitm_reply_buffer_index = 0;
+                }
+
+                break;
             default:
-                printf("event invalid 0x%02x\n", transfer->event);
                 break;
         }
         
